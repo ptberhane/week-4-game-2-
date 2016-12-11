6522a3bca1d144fa8b9fcd1338a10416800d52f8
ptberhane <piniel.berhane@gmail.com>
2016-12-11 00:31:09 -0500

    adding

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+var random = 26
+
+
+
+for(i = 0; i < random.length; i++){
+	var randomscore = random[i];
+	$("#random").append(randomscore)
+};
+
+
+
+var crystalval = [6,8,5,11];
+
+for (var i = 0; i < crystalval.length; i++) {
+	var gem = $("<img>");
+	 gem.addClass("gem-color");
+	 gem.attr("gem-value", crystalval[i]);
+	 gem.val(crystalval[i]);
+	 $(".gem").append(score);
+	}
+
+
+	$(".Gem").on("click"), function(){
+
+	}
+
+     
+ 	 
+$(".blue").on("click", function(){
+	var score = 8;
+}
+
+
+	
